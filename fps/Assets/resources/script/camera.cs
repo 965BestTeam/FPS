40c54be407d03ac737942681de302f9b196c8995
Yumuko-007 <957760578@qq.com>
2020-12-21 20:53:35 +0800

    first
    第一次上传

---
+++ fps/Assets/resources/script/camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Transform camTrans;
+    private Vector3 camAng;
+    private float camHeight = 2.5f;
+    void Rot_move()
+    {   
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+        camAng.x -= x;
+        camAng.y += y;
+        camTrans.eulerAngles = camAng;
+        camTrans.position = new Vector3(this.transform.position.x, camTrans.position.y, this.transform.position.z);
+        float camy = camAng.y;
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, camy, this.transform.eulerAngles.z);
+        Vector3 startPos = transform.position;
+        camTrans.position = startPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTrans = Camera.main.transform;
+        Vector3 startPos = transform.position; //位置
+        camTrans.position = startPos;
+        camTrans.rotation = transform.rotation;//角度
+        camAng = camTrans.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rot_move();
+    }
+}
---
+++ fps/Assets/resources/script/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 5;
+    public float mouseSpeed = 50;
+    public float jumpForce = 10000;
+    private new Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");//-1 ~ 1
+        float v = Input.GetAxis("Vertical");
+        // float mouse = Input.GetAxis("Mouse ScrollWheel");
+        this.transform.Translate(new Vector3(h * speed,0, v * speed) * Time.deltaTime, Space.World);
+        if (Input.GetButtonDown("Jump")){
+            rig.AddForce(new Vector3(0f, jumpForce, 0f));
+        }
+        this.transform.eulerAngles = transform.FindChild("Main Camera").transform.eulerAngles;
+    }
+}
